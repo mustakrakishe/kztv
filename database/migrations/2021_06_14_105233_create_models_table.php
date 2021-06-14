b6b5644eb1a25f276b66ca212c7a3aa106a7e910
mustakrakishe <mustakrkish@gmail.com>
2021-06-14 14:01:40 +0300

    Created: create_models_table migration.

---
+++ database/migrations/2021_06_14_105233_create_models_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateModelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('models', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('manufacture_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->string('name');
+            $table->foreignId('type_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('models');
+    }
+}
