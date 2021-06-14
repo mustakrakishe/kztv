52b970231b12a2c6c674eead75812ed485d1963b
mustakrakishe <mustakrkish@gmail.com>
2021-06-14 14:31:34 +0300

    Created: create_devices_table migration.

---
+++ database/migrations/2021_06_14_110156_create_devices_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDevicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->id();
+            $table->integer('inventory_code');
+            $table->integer('identificate_code')
+                ->nullable();
+            $table->foreignId('model_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('devices');
+    }
+}
