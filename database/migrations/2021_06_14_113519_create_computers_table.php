908e33603fc41560c9cfb02b777f5442debb6ad7
mustakrakishe <mustakrkish@gmail.com>
2021-06-14 14:53:11 +0300

    Created: create_computers_table migration.

---
+++ database/migrations/2021_06_14_113519_create_computers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComputersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('computers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('device_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->string('os', 30);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('computers');
+    }
+}
