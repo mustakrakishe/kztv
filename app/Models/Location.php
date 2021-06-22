c183991afcf2d77523479f59d37ea9787ea41353
mustakrakishe <mustakrkish@gmail.com>
2021-06-22 15:21:55 +0300

    UPD: Locaton model and migration. Location references to the subunits and department tables.

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Monitor extends Model
+class Location extends Model
 {
     use HasFactory;
 
