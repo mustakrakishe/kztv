357dfb5c77c72fbb68dc565f746fc0bc158b75c5
mustakrakishe <mustakrkish@gmail.com>
2021-06-22 14:19:19 +0300

    UPD: Renamed device_types and device_models migrations and modelt to product_types and product_models.

--- database/migrations/2021_06_14_104924_create_product_types_table.php
+++ database/migrations/2021_06_14_104924_create_product_types_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDeviceTypesTable extends Migration
+class CreateProductTypesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateDeviceTypesTable extends Migration
      */
     public function up()
     {
-        Schema::create('device_types', function (Blueprint $table) {
+        Schema::create('product_types', function (Blueprint $table) {
             $table->id();
             $table->string('name');
         });
@@ -26,6 +26,6 @@ class CreateDeviceTypesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('device_types');
+        Schema::dropIfExists('product_types');
     }
 }
--- database/migrations/2021_06_14_105233_create_product_models_table.php
+++ database/migrations/2021_06_14_105233_create_product_models_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDeviceModelsTable extends Migration
+class CreateProductModelsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateDeviceModelsTable extends Migration
      */
     public function up()
     {
-        Schema::create('device_models', function (Blueprint $table) {
+        Schema::create('product_models', function (Blueprint $table) {
             $table->id();
             $table->foreignId('manufacture_id')
                 ->constrained()
                 ->onUpdate('cascade')
                 ->onDelete('cascade');
             $table->string('name');
-            $table->foreignId('device_type_id')
+            $table->foreignId('product_type_id')
                 ->constrained()
                 ->onUpdate('cascade')
                 ->onDelete('cascade');
@@ -35,6 +35,6 @@ class CreateDeviceModelsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('device_models');
+        Schema::dropIfExists('product_models');
     }
 }
