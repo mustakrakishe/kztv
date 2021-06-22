db7846ae5c73282e54ee06ee30c4ce59420e3d15
mustakrakishe <mustakrkish@gmail.com>
2021-06-22 12:56:13 +0300

    Created: create_movement_logs_table migration.

---
+++ database/migrations/2021_06_22_075233_create_movement_logs_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMovementLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movement_logs', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('device_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreignId('location_id')
+                ->constrained('sqlsrv.[mx_kztu].[dbo].[Кадры_Подразделения]')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->text('cause');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movement_logs');
+    }
+}
