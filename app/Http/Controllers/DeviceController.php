81e7acbe1cb5b4a685fdb1bb31b2dc27f5a3b9fa
mustakrakishe <mustakrkish@gmail.com>
2021-06-30 11:05:02 +0300

    The device list is showed.

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -3,12 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+// use App\Models\Devices\Unit;
+// use App\Models\Devices\IdentificationCode;
+// use App\Models\Devices\Manufacture;
+// use App\Models\Devices\DeviceModel;
 
 class DeviceController extends Controller
 {
     public function show()
     {
-        return view('devices');
+        $modelsInfo = DB::table('device_models')
+                ->join('types', 'device_models.type_id', '=', 'types.id')
+                ->join('manufactures', 'device_models.manufacture_id', '=', 'manufactures.id')
+                ->select(
+                    'device_models.id as id',
+                    'manufactures.name as manufacture',
+                    'device_models.name as model'
+                );
+
+        $devices = DB::table('units')
+            ->leftJoin('identification_codes', 'units.id', '=', 'identification_codes.unit_id')
+            ->leftjoin('types', 'units.type_id', '=', 'types.id')
+            ->leftJoinSub($modelsInfo, 'modelsInfo', function ($leftJoin) {
+                $leftJoin->on('units.device_model_id', '=', 'modelsInfo.id');
+            })
+            ->select(
+                'units.inventory_code',
+                'identification_codes.value as identification_code',
+                'types.name as type',
+                'modelsInfo.manufacture',
+                'modelsInfo.model'
+            )
+            ->get();
+        return view('devices', ['devices' => $devices]);
     }
 
     public function add()
