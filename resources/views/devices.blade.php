53452cb27db15e2665a4ed7024c48e377c61d572
mustakrakishe <mustakrkish@gmail.com>
2021-07-08 09:26:04 +0300

    ADD: Add icon is added.

--- resources/views/devices.blade.php
+++ resources/views/devices.blade.php
@@ -6,16 +6,18 @@
         <div class="card-body">
             <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4">
                 <div class="row">
-                    <div class="col-sm-12 col-md-6">
+                    <div class="col-sm-11 col-md-6">
                         <div id="example1_filter" class="dataTables_filter">
                             <label>Пошук:<input type="search" class="form-control form-control-sm" placeholder="" aria-controls="example1"></label>
                         </div>
                     </div>
+                    <div class="col-sm-1 col-md-6 text-right mt-auto" style="padding: 8px 19.5px;">
+                        <i class="fas fa-plus"></i>
+                    </div>
                 </div>
                 <div class="row">
                     <div class="col-sm-12">
-                        <table id="example1" class="table table-striped dataTable dtr-inline" role="grid"
-                            aria-describedby="example1_info">
+                        <table class="table table-striped dataTable dtr-inline" role="grid" aria-describedby="example1_info">
                             <thead>
                                 <tr role="row">
                                     <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1">Інв. №</th>
