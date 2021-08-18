f353559149c0cc48c748c01d3be3dd89e2ffd774
mustakrakishe <mustakrkish@gmail.com>
2021-08-18 15:47:17 +0300

    DEL: Removed a dashboard route.

--- routes/web.php
+++ routes/web.php
@@ -16,18 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     if(Auth::check()){
-        return redirect('/dashboard');
+        return redirect('/devices');
     }
     else{
         return view('welcome');
     }
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})
-    ->middleware(['auth'])
-    ->name('dashboard');
 
 require __DIR__.'/auth.php';
 require __DIR__.'/devices.php';
